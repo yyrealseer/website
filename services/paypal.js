8cb6c45b165d404e83ee9ce7a23bfb1cae2e85d1
yyrealseer <xavier890930@gmail.com>
2024-11-15 14:58:27 +0800

    ECPay SQL test

--- services/paypal.js
+++ services/paypal.js
@@ -13,7 +13,7 @@ dotenv.config({ path: './.env.links' });
 
 // #region 初始化
 // PayPal
-const environment = new paypal.core.SandboxEnvironment(process.env.PAYPAL_CLIENT_ID, process.env.PAYPAL_CLIENT_SECRET);
+const environment = new paypal.core.LiveEnvironment(process.env.PAYPAL_CLIENT_ID, process.env.PAYPAL_CLIENT_SECRET);
 const client = new paypal.core.PayPalHttpClient(environment);
 
 // MongoDB
